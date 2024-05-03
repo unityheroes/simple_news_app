283fc19058361827d3a0df18c1b98a1038dcaff6
Ahmed Elhabak <ahmed.elhabak101@gmail.com>
2024-05-03 09:42:45 +0300

    refactor and enhance code

--- lib/widgets/new_list_view_builder.dart
+++ lib/widgets/new_list_view_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_news_app/models/article_model.dart';
+
 import 'package:simple_news_app/services/news_services.dart';
 import 'package:simple_news_app/widgets/news_list_view.dart';
 
@@ -13,38 +14,47 @@ class NewListViewBuilder extends StatefulWidget {
 }
 
 class _NewListViewBuilderState extends State<NewListViewBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
+  dynamic future;
   @override
   void initState() {
     super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsServices().getNews(category: NewsCategory.general);
-    isLoading = false;
-    setState(() {});
+    future = NewsServices().getNews(category: NewsCategory.technology);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) // loading indicator
-    {
-      return const SliverToBoxAdapter(
-          child: Center(child: CircularProgressIndicator()));
-    } else {
-      if (articles.isEmpty) // if list empity or error
-      {
-        return const SliverToBoxAdapter(
-          child: Center(child: Text("opps ! ! ! There Was Error  try later ")),
-        );
-      } else // if list have data
-      {
-        return NewsListView(
-          articles: articles,
-        );
-      }
-    }
+    return FutureBuilder<List<ArticleModel>>(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return NewsListView(articles: snapshot.data!); // Handling null data
+        } else if (snapshot.hasError) {
+          return const SliverToBoxAdapter(
+            child: Center(
+                child:
+                    Text("Oops! There was an error. Please try again later.")),
+          );
+        } else {
+          // Handling the case when there is neither data nor an error
+          return const SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
+    );
   }
 }
+// if (isLoading) // loading indicator
+    // {
+    //    else {
+    //   if (articles.isEmpty) // if list empity or error
+    //   {
+    //     return const SliverToBoxAdapter(
+    //       child: Center(child: Text("opps ! ! ! There Was Error  try later ")),
+    //     );
+    //   } else // if list have data
+    //   {
+    //     return NewsListView(
+    //       articles: articles,
+    //     );
+    //   }
